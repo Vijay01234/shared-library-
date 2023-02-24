19fa9bf974ab7ab2f9c68bdf9a9178be0a80c4a3
Vijay01234 <124332092+Vijay01234@users.noreply.github.com>
2023-02-24 10:55:21 +0530

    Update vijay.groovy

--- vars/vijay.groovy
+++ vars/vijay.groovy
@@ -1,9 +1,9 @@
 def sub(a,b,c)
 {
-   echo"we have ${a} and ${b}and ${a+b+c}
+   echo"we have ${a}and${b}and${c}and${a+b+c}
 }
 
 def add(a,b)
 {
-  echo"abcdef ${a} and ${b} then ${a-b}
+  echo"abcdef ${a}and ${b}then ${a-b}
 }
